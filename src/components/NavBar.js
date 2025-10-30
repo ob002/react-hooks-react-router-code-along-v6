f8e3fceff3a90c1755373b007fb7ef5761fec159
Amos Ombworo <amosombworo@gmail.com>
2025-10-31 01:45:24 +0300

    initial

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+    <nav>
+      <a className="nav-link" href="/">Home</a>
+      <a className="nav-link" href="/about">About</a>
+      <a className="nav-link" href="/login">Login</a>
+    </nav>
+  );
+};
+
+export default NavBar;                  
